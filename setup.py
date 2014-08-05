0e5c6a1bd0985e95c8fde83948e7a43c67c0b733
U-Asus\Gatien <gatien.bovyn@gmail.com>
2014-08-05 22:05:07 +0200

    [enh] Optimize Windows build

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from distutils.core import setup
 import py2exe
 
+to_exclude = ['_ssl', '_hashlib', 'doctest', 'difflib', 'pickle',
+'ftp_lib', 'ssl', 'cmd', 'unittest']
+
 setup(
-    options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
+    options = {'py2exe': {'bundle_files': 1, 'compressed': True, 'excludes': to_exclude, 'optimize': 2}},
     console = ['chamilo.py'],
     zipfile = None,
 )
